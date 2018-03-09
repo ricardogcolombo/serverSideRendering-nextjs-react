4886ab06c1219b761525cf3fd54738071e5275e8
Ricardo <R@Ricardo-3.local>
2018-03-09 16:10:41 -0300

    [ADD]load with async data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,28 @@
 import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI';
+import 'isomorphic-fetch';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
 
-const Index = ({
-        title = "Hello from next.js"
-    }) =>
+import RaisedButton from 'material-ui/RaisedButton';
+
+const Index = ({ posts }) => 
     <div>
-        <Header />
-        <h2>{title}</h2>
-    </div>
+    <Header />
+      {
+          posts.map(x => 
+            <Card key={x.id}> 
+                <CardHeader title={x.title} />
+                <CardText>
+                    <RaisedButton label="Click to view post!" fullWidth={true} primary={true} />
+                </CardText>
+            </Card>
+        )
+      }
+    </div>;
+Index.getInitialProps = async () => {
+    const response = await fetch(`${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`);
+    const data = await response.json();
+    return { posts: data.items }
+}
 
 export default withMui(Index);
