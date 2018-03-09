6eb3fce7a0d9ce8eca6c86b1ac35e377b56d4345
Ricardo <R@Ricardo-3.local>
2018-03-09 16:40:21 -0300

    [ADD]Posts details

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Header from '../components/header';
 import withMui from '../shared/MUI/withMUI';
 import 'isomorphic-fetch';
-import {Card, CardHeader, CardText} from 'material-ui/Card';
-
+import { Card, CardHeader, CardText } from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
+import Link from 'next/link';
 
 const Index = ({ posts }) => 
     <div>
@@ -13,12 +13,19 @@ const Index = ({ posts }) =>
             <Card key={x.id}> 
                 <CardHeader title={x.title} />
                 <CardText>
-                    <RaisedButton label="Click to view post!" fullWidth={true} primary={true} />
+                    <RaisedButton fullWidth={true} primary={true}>
+                    <Link href={`/post?id=${x.id}`} as={`/blog/${x.id}`}>
+                        <a>
+                            Click to view post!
+                        </a>
+                    </Link>
+                    </RaisedButton>
                 </CardText>
             </Card>
         )
       }
     </div>;
+
 Index.getInitialProps = async () => {
     const response = await fetch(`${process.env.BLOGGER_URL}?key=${process.env.API_KEY}`);
     const data = await response.json();
